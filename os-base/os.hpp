42a2f5e9599ab0b22b8de35effd0e3418f7c63f0
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-23 08:28:57 -0500

    Create os.hpp

---
+++ os-base/os.hpp
@@ -0,0 +1,24 @@
+//os.hpp
+
+#ifndef __OS_HPP__
+#define __OS_HPP__
+
+#define __OS_NAME__ "Mangoz OS"
+#define __OS_VERSION__ 1
+
+typedef bool boolean;
+
+class OS{
+public:
+  OS();
+  ~OS();
+
+  //NEVER CHANGE THE BOOT FILES NAME
+  const boolean __OS_BIOS_RUNNING__ ? 1 : 0; // is running?
+  const wchar_t * __OS_BOOT_FILE__ = "Mangoz_OS.asm";
+  void OS_CALL_BOOT_FILE(const wchar_t * __FILE__);
+  
+
+};
+
+#endif
